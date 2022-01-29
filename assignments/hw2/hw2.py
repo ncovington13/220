f80f5b49930617f17d94b2d728b6045336324974
nias13 <covingtonnm@g.cofc.edu>
2022-01-28 23:45:12 -0500

    Completed hw 2 (Nia)

--- assignments/hw2/hw2.py
+++ assignments/hw2/hw2.py
@@ -1,35 +1,61 @@
 """
-Name: <your name goes here – first and last>
-<ProgramName>.py
+Name: Nia Covington
+arthimetic.py
 
-Problem: <Brief, one or two sentence description of the problem that this program solves, in your own words.>
+Problem: These programs takes problems or questions and apply arithmetic methods to give outputs based on the inputs.
 
-Certification of Authenticity:
-<include one of the following>
+
 I certify that this assignment is entirely my own work.
-I certify that this assignment is my own work, but I discussed it with: <Name(s)>
 """
 import math
 
 
 def sum_of_threes():
-    pass
+    upper_bound = eval(input("What is the upper bound?"))
+    upper_for_three = upper_bound// 3
+    total_sum = 3* upper_for_three*(1+ upper_for_three)/2
+    # formula used to solve for the sum
+    print("The sum of three is", total_sum)
 
 
 def multiplication_table():
-    pass
-
+    for x in range (1, 11):
+        for y in range (1,11):
+            print(x*y, end=" ")
+        print()
 
 def triangle_area():
-    pass
+    side_a = float(input("What is the length of side A?"))
+    side_b = float(input("What is the length of side B?"))
+    side_c = float(input("What is the length of side C?"))
+    s = (side_a + side_b + side_c) / 2
+    # represents equation necessary for next part of the equation
+    area_1 = s - side_a
+    area_2 = s - side_b
+    area_3 = s - side_c
+    # represents different part of the area equation given
+    total_area = math.sqrt(s * area_1 * area_2 * area_3)
+    print("The area is", total_area)
+
 
 
 def sum_squares():
-    pass
-
+    lower_range = eval(input("Enter the lower range:"))
+    upper_range = eval(input("Enter the upper range:"))
+    total = 0
+    for i in range(lower_range,upper_range+1):
+        total = i * i
+    print(total)
 
 def power():
-    pass
+    base = int(input("Enter base:"))
+    exponent = int(input("Enter exponent:"))
+    ans=1
+    # accumulator for the answer to the power
+    for i in range(exponent):
+        ans=ans*base
+        # formula that gives the answer to the power
+    print(base,"^",exponent,"=", ans)
 
 
 if __name__ == '__main__':
