6f7e8b6e7b40fbc4cd3107f738803557a47eb705
nias13 <covingtonnm@g.cofc.edu>
2022-03-19 22:18:04 -0400

    Completed my final submission of hw 8; this is what I want to submit for my grade; re-do (Nia)

--- assignments/hw8/hw8.py
+++ assignments/hw8/hw8.py
@@ -88,46 +88,18 @@ def circle_overlap():
 
 
 def did_overlap(circle_one, circle_two):
-    width_px = 700
-    height_px = 700
-    win = GraphWin("Circle", width_px, height_px)
-    width = 10
-    height = 10
-    win.setCoords(0, 0, width, height)
-
-    center = win.getMouse()
-    circumference_point = win.getMouse()
-    radius = math.sqrt(
-        (center.getX() - circumference_point.getX()) ** 2 + (center.getY() - circumference_point.getY()) ** 2)
-    circle_one = Circle(center, radius)
-    circle_one.setFill("light blue")
-    circle_one.draw(win)
-
-    center_2 = win.getMouse()
-    circumference_point_2 = win.getMouse()
-    radius_2 = math.sqrt(
-        (center_2.getX() - circumference_point_2.getX()) ** 2 + (center_2.getY() - circumference_point_2.getY()) ** 2)
-    circle_two = Circle(center_2, radius_2)
-    circle_two.setFill('green')
-    circle_two.draw(win)
-
-    x_formula=abs((circle_one).getCenter().getX()-circle_two.getCenter().getX()**2)
-    y_formula=abs((circle_one.getCenter().getY()-circle_two.getCenter().getY())**2)
-    distance=math.sqrt(x_formula + y_formula)
-    if distance < 0:
-        msg=Point((100,300),"The circles overlap")
-        msg.draw(win)
+    circle_overlap()
+    radius=circle_one.getRadius()
+    radius2=circle_two.getRadius()
+    rad_sum= radius + radius2
+    center= circle_one.getCenter()
+    center2=circle_two.getCenter()
+    distance_part= (center2.getX()- center.getX() ** 2) + (center2.getY()-center.getY()**2)
+    final_distance=math.sqrt(distance_part)
+    if final_distance <=rad_sum:
+        return True
     else:
-        msg2=Point((50,350),"The circles do not overlap")
-        msg2.draw(win)
-
-    close=Text((Point(250,600),"Click anywhere to close!"))
-    close.setSize(20)
-    close.draw(win)
-    win.getMouse()
-    win.close()
-
-did_overlap()
+        return False
 
 
 if __name__ == '__main__':
