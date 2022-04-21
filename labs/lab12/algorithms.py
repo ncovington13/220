2f6baa423ab76742b9d93ae02e74fde42a3aa0e2
nias13 <covingtonnm@g.cofc.edu>
2022-04-20 22:10:19 -0400

    Completed re-do alab 12(Nia)

--- labs/lab12/algorithms.py
+++ labs/lab12/algorithms.py
@@ -5,6 +5,27 @@ Create different algorithms to figure out problems.
 I certify this is my own work.
 """
 from graphics import *
+def read_data(filename):
+    file=open(filename,"r")
+    read=file.readline()
+    split=read.split()
+    i=0
+    list= []
+    while read:
+        while i <len(split):
+            num=eval(split[i])
+            list.append(num)
+            i +=1
+        read=file.readline()
+        split=read.split()
+    return list
+
+def is_in_linear(search_val,values):
+    if search_val in values:
+        return True
+    else:
+        return False
+
 def selection_sort(values):
     for i in range(len(values)):
         minimum= i
--- labs/lab12/lab12.py
+++ labs/lab12/lab12.py
@@ -9,64 +9,56 @@ from random import randint
 def find_and_remove_first(list,value):
     i=0
     repeat=0
-    while i <= len(list):
+    while i < len(list):
         if list[i]==value and repeat==0:
             list.remove(value)
             list.insert(i,'Nia')
             repeat=repeat+1
         i=i+1
 
-
-def read_file(filename):
-    values=[]
-    i=0
-    file=open(filename,"r")
-    r_file=file.readlines()
-    while r_file:
-        a
-
-def is_in_linear(search_val,values):
-    if search_val in values:
-        return True
-    return False
-
 def good_input():
-    n=int(input("Enter number:"))
-    while n <1 or n>10:
-        if n<1:
-            print("Number is too low, should be greater than 0. Try again!")
+    user=eval(input("Enter number 1-10:"))
+    while user >=0 or user <=11:
+        if user<1:
+            print("Number is too low.")
+            user=eval(input('Enter a number 1-10:'))
+        elif user >10:
+            print("Number is too high.")
+            user=eval(input("Enter a number 1-10:"))
         else:
-            print("Number is too high, should be less than 10. Try again!")
-        n=int(input("Enter number:"))
+            return user
 
     return "good input"
 
 def num_digits():
-   prompt=int(input("Please input a positive interger"))
+   prompt= eval(input('Enter a positive integer:'))
    count=0
    while prompt !=0:
-       prompt//=10
-       count+=1
-   print("Number of digits:"+str(count))
+       count= count + 1
+       prompt //= 10
+   print("Number of digits:", count)
 
 def hi_lo_game():
     number=randint(1,100)
     i=0
-    while i <=7:
+    switch=False
+    while i !=7 and switch==False:
         prompt=int(input("Please guess the number:"))
-        if number== prompt:
-            print("Correct \n" "You win in",i+1,"guesses!")
-            continue
-        elif prompt>number:
+        if prompt> number:
+            i+=1
             print("too high")
-        elif prompt < number:
+        elif prompt< number:
+            i+=1
             print("too low")
         else:
-            print("Sorry, you lose. The number was",number)
-
+            switch=True
+            i +=1
+            print("You win in {} guesses".format(i))
+    else:
+        if i==7:
+            print("Sorry, you lose. The number was {}".format(number))
 
 if __name__=="__main__":
-    print("Testing function")
     print(good_input())
     num_digits()
     hi_lo_game()
